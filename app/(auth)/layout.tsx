e53177e976e6ec8f78a8240948ff8e37c2d5e741
ggaiti <ggaiti@hotmail.com>
2023-09-01 16:41:23 -0400

    implemented Clerk Auth and signup/in functionality

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+// This will allow much simpler and quicker SEO
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+
+export const metadata = {
+  title: "threads",
+  description: "A Next.js 13 Meta Threads Application",
+};
+const inter = Inter({ subsets: ["latin"] });
+export default function RootLayout({
+  children, // give prop
+}: {
+  children: React.ReactNode; //type of prop
+}) {
+  //returns layout
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          {/* everything that will be shown within this layout*/}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
