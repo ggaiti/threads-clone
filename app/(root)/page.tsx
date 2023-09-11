2027c5d0787eb09ae3feed9e201e611067e39022
ggaiti <ggaiti@hotmail.com>
2023-09-11 08:28:56 -0400

    creating functionality for thread card structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,12 @@
 // import { UserButton } from "@clerk/nextjs";
 
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   const result = await fetchPosts(1, 30);
   const user = await currentUser();
-  console.log(result);
 
   return (
     <>
@@ -22,7 +22,7 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id}
+                currentUserId={user?.id || ""}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
