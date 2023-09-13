ca2a278e160dcca8eea031165c4b05d6e9022fef
ggaiti <ggaiti@hotmail.com>
2023-09-13 10:38:12 -0400

    added typscript config in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  tyscript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
